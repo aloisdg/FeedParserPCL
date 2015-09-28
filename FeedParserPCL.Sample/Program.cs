16635029ee288e7e5ab24968040fbf34ff69361b
aloisdg <aloisdegouvello@live.fr>
2015-09-28 16:25:09 +0530

    Feat: Add unit tests and update the sample

--- FeedParserPCL.Sample/Program.cs
+++ FeedParserPCL.Sample/Program.cs
@@ -5,26 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
+using Nito.AsyncEx;
 
 namespace FeedParserPCL.Sample
 {
         internal class Program
         {
-                internal static void Main()
-                {
-	                const string rss = "https://www.reddit.com/r/csharp.rss";
-	                const string atom = "https://xkcd.com/atom.xml";
+		static void Main(string[] args)
+		{
+			AsyncContext.Run(() => MainAsync());
+		}
+
+		static async void MainAsync()
+		{
+			const string rss = "https://www.reddit.com/r/csharp.rss";
+			const string atom = "https://xkcd.com/atom.xml";
 			const string rdf = "http://planetrdf.com/guide/rss.rdf";
-                        var parser = new FeedParser();
 
-			var rssItems = parser.Parse(rss, FeedType.Rss);
-                        Console.WriteLine(rssItems.Count);
+			Func<string, Uri> toUri = url => new Uri(url, UriKind.Absolute);
+			var parser = new FeedParser();
 
-	                var atomItems = parser.Parse(atom, FeedType.Atom);
-			Console.WriteLine(atomItems.Count);
+			var rssItems = await parser.Parse(toUri(rss), FeedType.Rss);
+			Console.WriteLine(rssItems.Count());
 
-			var rdfItems = parser.Parse(rdf, FeedType.Rdf);
-			Console.WriteLine(rdfItems.Count);
-                }
+			var atomItems = await parser.Parse(toUri(atom), FeedType.Atom);
+			Console.WriteLine(atomItems.Count());
+
+			var rdfItems = await parser.Parse(toUri(rdf), FeedType.Rdf);
+			Console.WriteLine(rdfItems.Count());
+		}
         }
 }
---
+++ FeedParserPCL.Test/UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FeedParserPCL.Test
+{
+	[TestFixture]
+	public class UnitTest
+	{
+		private readonly Func<string, FeedType, int> _countItems = (content, type) => new FeedParser().Parse(content, type).Count();
+
+		[TestCase("https://www.reddit.com/r/csharp.rss", FeedType.Rss, 26, Description = "Test RSS")]
+		[TestCase("https://xkcd.com/atom.xml", FeedType.Atom, 4, Description = "Test ATOM")]
+		[TestCase("http://planetrdf.com/guide/rss.rdf", FeedType.Rdf, 10, Description = "Test RDF")]
+		public async void Test(string url, FeedType type, int expected)
+		{
+			var actual = await new FeedParser().Parse(new Uri(url, UriKind.Absolute), type);
+			Assert.AreEqual(actual.Count(), expected);
+		}
+
+		[TestCase(".rss", FeedType.Rss, Result = 26, Description = "Test RSS")]
+		[TestCase(".xml", FeedType.Atom, Result = 4, Description = "Test ATOM")]
+		[TestCase(".rdf", FeedType.Rdf, Result = 10, Description = "Test RDF")]
+		public int TestFromLocal(string extension, FeedType type)
+		{
+			var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\File\test" + extension);
+			var content = File.ReadAllText(path);
+			return _countItems(content, type);
+		}
+	}
+}
