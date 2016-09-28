d20aa65514ee01ab4b84227a7bfd380ae74a0157
aloisdg <aloisdegouvello@live.fr>
2016-09-28 18:59:27 +0200

    Refactor: Rename arguments. Add documentation. Improve style.

--- FeedParserPCL/FeedParser.cs
+++ FeedParserPCL/FeedParser.cs
@@ -9,19 +9,26 @@ namespace FeedParserPCL {
     /// A simple RSS, RDF and ATOM feed parser.
     /// </summary>
     public static class FeedParser {
-        public static IEnumerable<IItem> Parse(string content, FeedType feedType) {
-            var root = XDocument.Parse (content).Root;
+        /// <summary>
+        /// Parse a feed base on its own type.
+        /// </summary>
+        /// <param name="feed">the feed to be parsed</param>
+        /// <param name="type">the feed's own type</param>
+        /// <returns>The content as a enumerable of item</returns>
+        /// <example>
+        /// var feed = FeedParser.Parse (File.ReadAllText ("./feed.rss), FeedType.Rss);
+        /// foreach (var item in feed)
+        ///     Console.WriteLine ($"{item.PublishDate} {item.Title}");
+        /// </example>
+        public static IEnumerable<IItem> Parse(string feed, FeedType type) {
+            var root = XDocument.Parse (feed).Root;
             if (root == null)
                 throw new FormatException (nameof (root));
-            switch (feedType) {
-                case FeedType.Atom:
-                    return new Atom (root);
-                case FeedType.Rss:
-                    return new Rss (root);
-                case FeedType.Rdf:
-                    return new Rdf (root);
-                default:
-                    throw new NotSupportedException (feedType + " is not supported");
+            switch (type) {
+                case FeedType.Atom: return new Atom (root);
+                case FeedType.Rss: return new Rss (root);
+                case FeedType.Rdf: return new Rdf (root);
+                default: throw new NotSupportedException (type + " is not supported");
             }
         }
     }
